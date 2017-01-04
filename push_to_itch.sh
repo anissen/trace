11840abd637c0fecf5a46486e6773eb7ce0875d2
Anders Nissen <andnis@gmail.com>
2017-01-04 20:07:43 +0100

    Itch!

---
+++ push_to_itch.sh
@@ -0,0 +1,5 @@
+flow build web
+/Users/nissen/Library/Application\ Support/itch/bin/butler push bin/web anissen/trace:web
+
+flow build mac
+/Users/nissen/Library/Application\ Support/itch/bin/butler push bin/mac64 anissen/trace:mac
