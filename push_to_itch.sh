eac94130baec741a53c7e369a0381fd6abcc65c6
Anders Nissen <andnis@gmail.com>
2017-01-04 20:15:05 +0100

    Use symlink

--- push_to_itch.sh
+++ push_to_itch.sh
@@ -1,5 +1,5 @@
 flow build web
-/Users/nissen/Library/Application\ Support/itch/bin/butler push bin/web anissen/trace:web
+butler push bin/web anissen/trace:web
 
 flow build mac
-/Users/nissen/Library/Application\ Support/itch/bin/butler push bin/mac64 anissen/trace:mac
+butler push bin/mac64 anissen/trace:mac
